09e119f99332a0d96501ce37a7e5f6b2fa510304
Nikolay <kolekdoma@mail.ru>
2023-03-15 21:59:06 +0300

    Create project

---
+++ Mobile_MauiApp/Mobile_MauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mobile_MauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mobile_MauiApp/Mobile_MauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mobile_MauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
