84093abff4ec170531621f7e4324b7ca2de387de
Nikolay <kolekdoma@mail.ru>
2023-03-15 22:09:49 +0300

    Build project

---
+++ Mobile_MauiApp/Mobile_MauiApp/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Mobile_MauiApp.MainApplication, Mobile_MauiApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6418c3199690c515ac.MainApplication.class, crc6418c3199690c515ac.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
--- Mobile_MauiApp/Mobile_MauiApp/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/Mobile_MauiApp.AssemblyInfo.cs
+++ Mobile_MauiApp/Mobile_MauiApp/obj/Debug/net6.0-windows10.0.19041.0/win10-x64/Mobile_MauiApp.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "9abf1f04-6930-4e9a-b44c-985011a8fb70")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "Mobile_MauiApp")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mobile_MauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mobile_MauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mobile_MauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
